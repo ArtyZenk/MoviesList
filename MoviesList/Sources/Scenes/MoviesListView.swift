f98a7438144a2bbc402f806b4212fe6f8d939bd6
Artyom  Guzenko <artyomka13@gmail.com>
2023-09-25 14:42:47 +0300

    add tableView

--- MoviesList/Sources/Scenes/MoviesListView.swift
+++ MoviesList/Sources/Scenes/MoviesListView.swift
@@ -9,12 +9,65 @@ import UIKit
 import SnapKit
 
 class MoviesListView: UIViewController {
-
+    
+    // MARK: - Outlets
+    
+    private lazy var moviesTable: UITableView = {
+        let table = UITableView(frame: .zero, style: .plain)
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "movies")
+        table.dataSource = self
+        table.delegate = self
+        return table
+    }()
+    
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        setupHierarch()
+        setupLayout()
+        setupView()
     }
 }
 
+// MARK: - Configuration View
+
+extension MoviesListView {
+    private func setupHierarch() {
+        view.addSubview(moviesTable)
+    }
+    
+    private func setupLayout() {
+        moviesTable.snp.makeConstraints { make in
+            make.top.bottom.left.right.equalTo(view)
+        }
+    }
+    
+    private func setupView() {
+    }
+}
+
+// MARK: - UITableViewDelegate
+extension MoviesListView: UITableViewDelegate {
+
+}
+
+// MARK: - UITableViewDataSource
+extension MoviesListView: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "movies", for: indexPath)
+        var content = cell.defaultContentConfiguration()
+        content.text = "cell \(indexPath.row)"
+        content.image = UIImage(named: "1")
+        cell.contentConfiguration = content
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        100
+    }
+}
